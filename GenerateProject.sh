628fe79572a5b70d60184ea215aa9fbbf591ec64
Khalil Hajlaoui <khalil.hajlaoui@gmail.com>
2019-11-10 23:36:03 -0500

    [Global] add cmake project script generator

---
+++ GenerateProject.sh
@@ -0,0 +1,11 @@
+echo "Cmake generate project"
+
+echo "step 1 - remove Build directory"
+rm -r Build
+
+echo "step 2 - create Build directory"
+mkdir Build
+
+echo "step 3 - generate project"
+cd Build/
+cmake ../Source/
