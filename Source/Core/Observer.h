155a19510b7975e0ea88bbd8431b7dde97eb02fb
Khalil Hajlaoui <khajlaoui@novaquark.com>
2020-09-09 21:50:52 -0400

    [Core] add Event Classes files
    + create Observer event
    + create Queue/Broadcast event
    ~ migrate logic to separate classes
    ~ define generic callback message
    - remove duplicate logic

---
+++ Source/Core/Observer.h
@@ -0,0 +1,153 @@
+#pragma once
+#include <memory>
+#include <unordered_map>
+#include "Event.h"
+
+namespace Event
+{
+    class Subject;
+
+    struct SubjectHash
+    {
+        size_t operator()(const std::reference_wrapper<Subject>& key) const;
+    };
+
+    struct SubjectEqual
+    {
+        bool operator()(const std::reference_wrapper<Subject>& key,
+            const std::reference_wrapper<Subject>& key2) const;
+    };
+
+    // dedicated to be a linked list
+    class Observer
+    {
+        using SubjectRef = std::reference_wrapper<Subject>;
+        using SubjectMap = std::unordered_map<SubjectRef, CallBack, SubjectHash, SubjectEqual>;
+
+        friend class Subject;
+    public:
+        virtual ~Observer();
+        
+    private:
+        
+        SubjectMap _callbackMap;
+        std::weak_ptr<Observer> _next;
+
+        void process(Subject &reference)
+        {
+            auto it = _callbackMap.find(std::ref(reference));
+            if (it != _callbackMap.end())
+            {
+                it->second();
+            }
+        }
+
+        void clear(Subject &reference)
+        {
+            _callbackMap.erase(std::ref(reference));
+            _next.reset();
+        }
+    };
+
+    class Subject
+    {
+    public:
+        virtual ~Subject()
+        {
+            clear();
+        }
+   
+        void subscribe(Observer &observer, CallBack cb)
+        {
+            observer._callbackMap.emplace(std::make_pair(std::ref(*this), std::move(cb)));
+            observer._next = std::move(_head);
+            _head = std::make_shared<Observer>(observer);
+        }
+
+        void unsubscribe(Observer &observer)
+        {
+            if (auto pHead = _head.lock())
+            {
+                if (pHead.get() == &observer)
+                {
+                    _head = std::move(observer._next);
+                    observer.clear(*this);
+                    return;
+                }
+
+                auto it = pHead;
+                while (it)
+                {
+                    auto pNext = it->_next.lock();
+                    if (pNext)
+                    {
+                        if (pNext.get() == &observer)
+                        {
+                            it->_next = std::move(observer._next);
+                            observer.clear(*this);
+                            return;
+                        }
+                    }
+
+                    it = pNext;
+                }
+            }
+        }
+
+        void operator()()
+        {
+            if (auto pHead = _head.lock())
+            {
+                auto it = pHead;
+                while (it)
+                {
+                    it->process(*this);
+
+                    it = it->_next.lock();
+                }
+            }
+        }
+
+        void clear()
+        {
+            if (auto pHead = _head.lock())
+            {
+                auto it = pHead;
+                while (it)
+                {
+                    auto pNext = it->_next.lock();
+                    if (pNext)
+                        it->clear(*this);
+
+                    it = pNext;
+                }
+            }
+
+            _head.reset();
+        }
+
+    private:
+        std::weak_ptr<Observer> _head;
+    };
+
+    Observer::~Observer()
+    {
+        for (const auto& entry : _callbackMap)
+        {
+            entry.first.get().unsubscribe(*this);
+        }
+
+        _callbackMap.clear();
+    }
+
+    size_t SubjectHash::operator()(const std::reference_wrapper<Subject>& key) const
+    {
+        return std::hash<Subject*>()(&key.get());
+    }
+
+    /// We check here the equality of the reference and not the object's value itself
+    bool SubjectEqual::operator()(const std::reference_wrapper<Subject>& key, const std::reference_wrapper<Subject>& key2) const
+    {
+        return &key.get() == &key2.get();
+    }
+}
